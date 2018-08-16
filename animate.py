8f2f462efb711bf2729b94a3b67e66a672bbf6f1
Alora Boatwright-Mars <iamblackeclipse@gmail.com>
2018-08-16 10:25:04 -0700

    incomplete animation

--- animate.py
+++ animate.py
@@ -10,25 +10,73 @@ img1 = pygame.image.load("sprites/running/fox1running.png")
 display_width = 1000
 display_height = 600
 
-gameDisplay = pygame.display.set_mode((display_width, display_height))
-
 sprite_image5 = pygame.image.load("sprites/running/fox5running.png")
 sprite_image4 = pygame.image.load("sprites/running/fox4running.png")
 sprite_image3 = pygame.image.load("sprites/running/fox3running.png")
-sprite_image2 = pygame.image.load("sprites/running/fox2running.png")
+sprite_image2 = pygame.image.load("sprites/running/fox5running.png")
 sprite_image1 = pygame.image.load("sprites/running/fox1running.png")
 
 rect = sprite_image1.get_rect()
-gameDisplay.blit(sprite_image1, rect)
-gameDisplay.blit(sprite_image2, rect)
-gameDisplay.blit(sprite_image3, rect)
-gameDisplay.blit(sprite_image4, rect)
-gameDisplay.blit(sprite_image5, rect)
+# gameDisplay.blit(sprite_image1, rect)
+# gameDisplay.blit(sprite_image2, rect)
+# gameDisplay.blit(sprite_image3, rect)
+# gameDisplay.blit(sprite_image4, rect)
+# gameDisplay.blit(sprite_image5, rect)
+#
+# while True: #main game loop
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#             pygame.quit()
+#             sys.exit()
+#
+#     pygame.display.update()
 
-while True: #main game loop
-    for event in pygame.event.get():
-        if event.type == QUIT:
+# ---------------------------------------------------------
+
+def load_image(name):
+    image = pygame.image.load(name)
+    return image
+
+class TestSprite(pygame.sprite.Sprite):
+    def __init__(self):
+        super(TestSprite, self).__init__()
+        self.images = []
+        self.images.append(load_image("sprites/running/fox1running.png"))
+        self.images.append(load_image("sprites/running/fox2running.png"))
+        self.images.append(load_image("sprites/running/fox3running.png"))
+        self.images.append(load_image("sprites/running/fox4running.png"))
+        self.images.append(load_image("sprites/running/fox5running.png"))
+
+
+
+        self.index = 0
+        self.image = self.images[self.index]
+        self.rect = pygame.Rect(5, 5, 64, 64)
+
+    def update(self):
+        self.index += 1
+        if self.index >= len(self.images):
+            self.index = 0
+        self.image = self.images[self.index]
+
+def main():
+    pygame.init()
+    gameDisplay = pygame.display.set_mode((display_width, display_height))
+    my_sprite = TestSprite()
+    my_group = pygame.sprite.Group(my_sprite)
+
+    while True:
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            sys.exit(0)
 
-    pygame.display.update()
+        # Calling the 'my_group.update' function calls the 'update' function of all
+        # its member sprites. Calling the 'my_group.draw' function uses the 'image'
+        # and 'rect' attributes of its member sprites to draw the sprite.
+        my_group.update()
+        my_group.draw(gameDisplay)
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
